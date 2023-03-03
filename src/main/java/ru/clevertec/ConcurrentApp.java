f304be097281d87d3517575be94602e831c69d2a
Pavel Lukyanovich <paellukyanovich1@gmail.com>
2023-03-03 12:35:20 +0300

    refactor: rename main class

--- src/main/java/ru/clevertec/ConcurrentApp.java
+++ src/main/java/ru/clevertec/ConcurrentApp.java
@@ -8,7 +8,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class Client1 {
+public class ConcurrentApp {
 
     public static void main(String[] args) {
         System.out.println("Main start");
